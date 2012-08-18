40ebec1dc9a6938e63032c99a20de3636c3ac96d
John F. Brown <jack@brownjohnf.com>
2012-08-18 16:18:44 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeaceCorpsAfrica::Application.config.secret_token = '6a2851b0e2d2125c0a2c3a8e4c404e9674cd7e375d7e8394c4174d4ed76188dcae53347a70f3a8c7bb3a5a161f342c2cc31131ee53c7a791ae8503d09ae74fad'
