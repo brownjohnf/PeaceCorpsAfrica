1be89d5382280c23a4faf670fc465b1a82c4fbbf
John F. Brown <jack@brownjohnf.com>
2012-08-19 16:01:09 +0000

    added lots of colors; working on devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 PeaceCorpsAfrica::Application.routes.draw do
+
   authenticated :user do
     root :to => 'home#index'
   end
+  
   root :to => "home#index"
+  
   devise_for :users
+  
   resources :users, :only => [:show, :index]
+
 end
