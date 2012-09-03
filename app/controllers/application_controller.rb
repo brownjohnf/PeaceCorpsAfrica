cf0940bf6463699cb05b763c2780039abb941f00
John F. Brown <jack@brownjohnf.com>
2012-09-03 14:40:29 +0000

    set country/region by country_code if present, save as cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :handle_cookies
+
   protect_from_forgery
 
   check_authorization :unless => :devise_controller?
@@ -6,4 +9,13 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  def handle_cookies
+    if params[:country_code]
+      cookies[:country] = Carmen.country_name(params[:country_code])
+    else
+      cookies[:country] = 'Africa'
+    end
+  end
+
 end
