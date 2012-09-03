a63ddf2ba1467af068cdb3eb04abf7885a735869
John F. Brown <jack@brownjohnf.com>
2012-09-03 19:45:42 +0000

    allowed anyone to read everything

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
     if user.has_role? :admin
       can :manage, :all
     else
-      can :read, Country
+      can :read, :all
     end
 
     # Define abilities for the passed in user here. For example:
