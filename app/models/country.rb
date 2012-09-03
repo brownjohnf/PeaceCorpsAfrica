0863de25930f43070bb26c8aa773e387d0f839c5
John F. Brown <jack@brownjohnf.com>
2012-09-03 10:47:58 +0000

    fixed validation; updated valid start date

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,10 +3,11 @@ class Country < ActiveRecord::Base
 
   attr_accessible :code, :name, :page_id, :active, :num_volunteers, :num_total_volunteers, :contact_email, :site_url, :donate_url, :pc_start_date, :country_initiatives_attributes
 
+  # required: name, pc_start_date
   validates :name, :code, :pc_start_date, :presence => true
   validates :name, :contact_email, :site_url, :donate_url, :length => { :maximum => 255 }
   validates :code, :length => { :minimum => 2, :maximum => 2 }
-  validate :valid_date
+  validate :valid_date, :unless => 'pc_start_date.blank?'
 
   before_validation :do_before_validation
 
@@ -29,12 +30,12 @@ class Country < ActiveRecord::Base
   private
 
     def valid_date
-      errors.add('PC Start Date', 'must be later than 1961') unless pc_start_date > '1963-01-01'.to_date
+      errors.add('PC Start Date', 'must be later than 1961') unless pc_start_date > '1961-01-01'.to_date
     end
 
     def do_before_validation
       self.name = Carmen.country_name(code)
-      self.pc_start_date = pc_start_date.to_date
+      self.pc_start_date = pc_start_date.to_date unless pc_start_date.blank?
     end
 
 end
