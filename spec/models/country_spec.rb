65a99812aa794ff325e2a0012eb05d435a5c7b2a
John F. Brown <jack@brownjohnf.com>
2012-09-04 13:22:55 +0000

    adjusted for new start_date parameter

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Country do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @attr = {
+      :code => 'SN',
+      :start_date => '1961-01-02'
+    }
+  end
+
+  it 'should create a new instance given valid attributes' do
+    Country.create! @attr
+  end
+
 end
