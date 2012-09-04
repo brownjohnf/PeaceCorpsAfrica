09429973997997f37b66b3433546dc8dcc16f1e3
John F. Brown <jack@brownjohnf.com>
2012-09-04 12:18:44 +0000

    removed comment-out for dropdown; fixed by deleted public/assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,5 @@ PeaceCorpsAfrica::Application.configure do
 
   # Expands the lines which load the assets
   # COMMENTED OUT by jack because for some reason this breaks dropdowns in bootstrap-sass 2.0.4.2
-  # config.assets.debug = true
+  config.assets.debug = true
 end
