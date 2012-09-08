ac9bf19a138587a43a8a825edfb97b4128527400
John F. Brown <jack@brownjohnf.com>
2012-09-08 00:37:47 +0000

    added markdown initializer; added markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # @author John Brown
+  # Converts Markdown text to HTML.
+  #
+  # @param [String] Markdown
+  #
+  # @return [String] HTML
+  #
+  def markdown(text)
+    Markdown.render(text).html_safe
+  end
+
 end
